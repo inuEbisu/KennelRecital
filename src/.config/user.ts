f2ee84209e65acbfd2161da551d765b86028aa0e
inuEbisu <inuebisu@outlook.com>
2025-09-15 16:27:03 +0800

    Set up basic information

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -2,6 +2,27 @@ import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
   // Override the default config here
-  // site: { title: "講評世界" },
-  // seo: { twitter: "@moeyua13" },
+  site: {
+    website: "https://inuebisu.cn",
+    title: "犬窝闲谭",
+    subtitle: "KennelRecital",
+    author: "inuEbisu",
+    description: "inuEbisu's personal blog",
+    navLinks: [
+      { name: '首页', href: '/' },
+      { name: '博客', href: '/archive' },
+      { name: '伙伴', href: '/friends' },
+    ],
+    socialLinks: [
+      { name: 'email', href: 'mailto:inuebisu@outlook.com' },
+      { name: 'github', href: 'https://github.com/inuEbisu' },
+      // { name: 'notebook', href: 'https://note.inuebisu.cn'},
+    ],
+    footer: [
+      '© 2020-2025 <a target="_blank" href="%website">%author</a>',
+    ],
+  },
+  seo: {
+    twitter: "@inuEbisu_3rd",
+  },
 }
