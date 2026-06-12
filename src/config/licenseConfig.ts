f9408309b2e954c84a0568bc0064fee17f6cb1b7
inuEbisu <inuebisu@outlook.com>
2026-06-12 22:11:32 +0800

    chore: update license

--- src/config/licenseConfig.ts
+++ src/config/licenseConfig.ts
@@ -5,6 +5,6 @@ export const licenseConfig: LicenseConfig = {
 	enable: true,
 
 	// 许可证名称及链接
-	name: "CC BY-NC-SA 4.0",
-	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+	name: "CC BY-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-sa/4.0",
 };
